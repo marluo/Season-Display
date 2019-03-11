f62f2384c9a4a0c9cbbe7e5c1d0a0cd77a7e075c
Marcus Lundgren <lundgren90@linuxmail.org>
2019-03-11 20:57:40 +0100

    Finished app.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Vi skickar datan till vår "Childs"
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err =>
+        this.setState({ errorMessage: err.message, spinnerLoader: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+      {
+        /*the new value will be put into Seasn display, and rerender it aswell. It will also rerender the children*/
+      }
+    }
+    return <Spinner message="Please accept a location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+Spinner.defaultProps = {
+  message: "Loading..."
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
